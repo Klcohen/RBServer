9030af445b6c508377015d7a8681483c943015dc
Keve Cohen <klcohen@iu.edu>
2019-07-11 14:11:23 -0400

    art controller done

--- server/controllers/artController.js
+++ server/controllers/artController.js
@@ -16,4 +16,36 @@ router.post("/", validateSession, (req, res) => {
     .catch(err => res.status(500).json({error: err}));
 });
 
+// art/mine  a feed of your art
+router.get("/mine", validateSession,  (req, res) => {
+      Art.findAll({ where: { owner: req.user.id}})
+          .then(art => res.status(200).json(art))
+          .catch(err => res.status(500).json({error: err}))
+  
+  });
+
+  // art/feed  a feed of all art
+router.get("/feed", validateSession,  (req, res) => {
+    Art.findAll()
+        .then(art => res.status(200).json(art))
+        .catch(err => res.status(500).json({error: err}))
+
+});
+
+//EDIT
+router.put('/:id', validateSession, (req, res) => {
+    Art.update(req.body.art, { where: { id: req.params.id, owner: req.user.id},returning: true})
+      .then(art => res.status(200).json(art))
+      .catch(err => res.status(500).json({ error: err}))
+  });
+
+  // DELETE
+// Allows individual feeds to be deleted by a user.
+
+router.delete('/:id', validateSession, (req, res) => {
+    Art.destroy({where:  {id: req.params.id, owner: req.user.id}})
+      .then(art => res.status(200).json(art))
+      .catch(err => res.status(500).json({ error: err}))
+  })
+
 module.exports = router;
